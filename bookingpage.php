967054caae94ef52b180fb030d1d89e3262b5a9e
sabrineabd <38770178+sabrineabd@users.noreply.github.com>
2018-12-08 11:22:43 -0500

    Update bookingpage.php

--- bookingpage.php
+++ bookingpage.php
@@ -1,28 +1,7 @@
 <?php
     session_start();
-
-    $origin = $_SESSION["origin"];
-    $destination = $_SESSION["destination"];
     // $departdate = $_SESSION["departdate"];
-
-    $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
-    $conn = new PDO("mysql:host=localhost;dbname = flightdb", "root", "", $options);
-    $sqlquery = "SELECT flight_times, flight_duration, airline
-                  FROM flightdb.flightinfo
-                  WHERE flight_origin = '".$origin."'
-                  AND flight_destination = '".$destination."'";
-    $results = $conn->query($sqlquery);
-    $secondquery = "SELECT flight_times, flight_duration, airline
-                  FROM flightdb.flightinfo
-                  WHERE flight_origin = '".$destination."'
-                  AND flight_destination = '".$origin."'";
-    $secondresults = $conn->query($secondquery);
-
-
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head> <title> Booking Page </title>
@@ -38,6 +17,38 @@
 </head>
 
 <body>
+  <?php
+      $origin = $destination = "";
+      if(isset($_SESSION["origin"])){
+        $origin = $_SESSION["origin"];
+      }
+      if(isset($_SESSION["destination"])){
+        $destination = $_SESSION["destination"];
+      }
+      if(isset($_SESSION["depart_date"])){
+        $depart_date = $_SESSION["depart_date"];
+      }
+      if(isset($_SESSION["return_date"])){
+        $return_date = $_SESSION["return_date"];
+      }
+   ?>
+   <?php
+
+       $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+       $conn = new PDO("mysql:host=localhost;dbname = flightdb", "root", "", $options);
+       $sqlquery = "SELECT flight_times, flight_duration, airline
+                     FROM flightdb.flightinfo
+                     WHERE flight_origin = '".$origin."'
+                     AND flight_destination = '".$destination."'";
+       $results = $conn->query($sqlquery);
+       $secondquery = "SELECT flight_times, flight_duration, airline
+                     FROM flightdb.flightinfo
+                     WHERE flight_origin = '".$destination."'
+                     AND flight_destination = '".$origin."'";
+       $secondresults = $conn->query($secondquery);
+
+
+   ?>
 
   <header>
     <h1 class="slogan">FlyEasy</h1>
@@ -52,15 +63,15 @@
               echo "<table>";
                 echo "<tr>";
                   echo "<td> <img src='".$row['airline'].".png' width='40px' height='40px'> </td>";
-                  echo "<th> date </th>";
+                  echo "<th>".$depart_date."</th>";
                   echo "<th>".strtoupper($origin)."</th>";
                   echo "<th>".$row['flight_times']."</th>";
                   echo "<th>".$row['flight_duration']."</th>";
-                  echo "<th class='price'>$".rand(300,700)."</th>";
+                  echo "<th class='price'>$".rand(100,400)."</th>";
                 echo "</tr>";
                 echo "<tr>";
                   echo "<td> <img src='".$row2['airline'].".png' width='40px' height='40px'> </td>";
-                  echo "<th> date </th>";
+                  echo "<th>".$return_date."</th>";
                   echo "<th>".strtoupper($destination)."</th>";
                   echo "<th>".$row2['flight_times']."</th>";
                   echo "<th>".$row2['flight_duration']."</th>";
@@ -80,4 +91,3 @@
 
 </body>
 <html>
-
